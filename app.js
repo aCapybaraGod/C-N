0a0f9493e55ccee69bdc2a76cd6c44582d62d7ba
aCapybaraGod <wsmet2012@gmail.com>
2026-05-23 17:27:40 +0200

    Update Google Form URL in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Google Form URL for all bracelets
-  const formUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSfaPygXwrGUlQxLEfmEH4FJIf50Z69JdOibsbEt6bV57QpfDw/viewform?usp=preview';
+  const formUrl = 'https://surveyheart.com/form/6a11c6ab0f3f0bf1641ae7e8';
 
   // Attach click handler to all "Buy Now" buttons (secondary buttons)
   document.querySelectorAll('.button-secondary').forEach(btn => {
